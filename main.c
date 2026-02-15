fe15b0f12b91a2bba982c1898508a16bdcd3f9bd
Phil Glazzard <phil@hifigear.co.uk>
2026-02-15 19:18:21 +0000

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#define LED_GPIO 18
+static const char *TAG = "LED_TEST"; 
+
+void app_main(void)
+{
+    
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+
+   // uint32_t led_state = 0; // keep track of the led state
+   bool led_state = 0; // keep track of the led state
+
+    while (1) {
+            // Toggle the LED state
+            led_state = !led_state;
+            // Drive the led
+            gpio_set_level(LED_GPIO, led_state);
+            // Feedback on terminal
+            ESP_LOGI(TAG,"Status:%s", led_state? "ON":"OFF");
+            // Delay of 0.5s
+            vTaskDelay(pdMS_TO_TICKS(500));
+    }
+
+}
